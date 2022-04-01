9af836eb19273d85f994aecefab065ad68ff937e
Paolo Di Francesco <frpaolo@amazon.at>
2022-04-01 11:36:17 +0000

    Update train.py: log to Experiments
    * log MLFlow specifics to the TrialComponent
    * dump SM specifics into MLFlow
    * update artifacts outputs in TrialComponent

--- lab/nginxBasicAuth/source_dir/train.py
+++ lab/nginxBasicAuth/source_dir/train.py
@@ -7,11 +7,17 @@ import argparse
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
+
 import mlflow
 import mlflow.sklearn
+from mlflow.tracking import MlflowClient
+
 import joblib
 import boto3
 import json
+import time
+
+from smexperiments.tracker import Tracker
 
 logging.basicConfig(level=logging.INFO)
 
@@ -30,6 +36,15 @@ def retrieve_credentials(region_name, secret_name):
     credentials['password'] = json.loads(secret['SecretString'])['password']
     
     return credentials
+
+def print_auto_logged_info(r):
+    tags = {k: v for k, v in r.data.tags.items()}
+    artifacts = [f.path for f in MlflowClient().list_artifacts(r.info.run_id, "model")]
+    print("run_id: {}".format(r.info.run_id))
+    print("artifacts: {}".format(artifacts))
+    print("params: {}".format(r.data.params))
+    print("metrics: {}".format(r.data.metrics))
+    #print("tags: {}".format(tags))
     
 if __name__ =='__main__':
     parser = argparse.ArgumentParser()
@@ -49,6 +64,7 @@ if __name__ =='__main__':
     parser.add_argument('--test', type=str, default=os.environ.get('SM_CHANNEL_TEST'))
     parser.add_argument('--train-file', type=str, default='california_train.csv')
     parser.add_argument('--test-file', type=str, default='california_test.csv')
+    parser.add_argument('--user', type=str, default='sagemaker')
     parser.add_argument('--features', type=str)  # we ask user to explicitly name features
     parser.add_argument('--target', type=str) # we ask user to explicitly name the target
 
@@ -72,9 +88,11 @@ if __name__ =='__main__':
 
     # set remote mlflow server
     mlflow.set_tracking_uri(args.tracking_uri)
-    mlflow.set_experiment(args.experiment_name)
-    
-    with mlflow.start_run():
+    experiment = mlflow.set_experiment(args.experiment_name)
+
+    mlflow.autolog()
+
+    with mlflow.start_run() as run:
         params = {
             "n-estimators": args.n_estimators,
             "min-samples-leaf": args.min_samples_leaf,
@@ -103,3 +121,52 @@ if __name__ =='__main__':
         # SAVE MODEL
         logging.info('saving model in MLflow')
         mlflow.sklearn.log_model(model, "model")
+        sm_data = json.loads(os.environ.get('SM_TRAINING_ENV'))
+        job_name = sm_data['job_name']
+        
+        # Overwrite system tags
+        mlflow.set_tags(
+            {
+                'mlflow.source.name': f"https://{args.region}.console.aws.amazon.com/sagemaker/home?region={args.region}#/jobs/{job_name}",
+                'mlflow.source.type': 'JOB',
+                'mlflow.user': args.user
+            }
+        )
+        # Shovel all SageMaker related data into mlflow
+        mlflow.set_tags(sm_data)
+
+    run_id = run.info.run_id
+    experiment_id = experiment.experiment_id
+
+    r = mlflow.get_run(run_id=run_id)
+    print_auto_logged_info(r)
+
+    artifacts = [f.path for f in MlflowClient().list_artifacts(r.info.run_id, "model")]
+
+    tracker_parameters = {
+            "run_id": run_id,
+            "experiment_id": experiment_id,
+            "mlflow-run-url": f"{args.tracking_uri}/#/experiments/{experiment_id}/runs/{run_id}"
+        }
+    try:
+        with Tracker.load() as tracker:
+            tracker.log_parameters(tracker_parameters)
+            tracker.log_parameters(r.data.params)
+            for metric_name, value in r.data.metrics.items():
+                tracker.log_metric(metric_name=metric_name, value=value)
+            for artifact in artifacts:
+                tracker.log_output(name=f"MLFlow.{artifact}", value=f"{r.info.artifact_uri}/{artifact}")
+            # Nullify default SageMaker.ModelArtifact
+            tracker.log_output(name="SageMaker.ModelArtifact", value="NA")
+        print("Loaded existing tracker")
+    except:
+        print("Could not load tracker (likely running in local mode). Create a new one")
+        create_date = time.strftime("%Y-%m-%d-%H-%M-%S")
+        tracker_name = f"mlflow-tracker-{create_date}"
+        with Tracker.create(display_name=tracker_name) as tracker:
+            tracker.log_parameters(tracker_parameters)
+            tracker.log_parameters(r.data.params)
+            print("Metric cannot be logged when creating a tracker in this way")
+            for artifact in artifacts:
+                tracker.log_output(name=f"MLFlow.{artifact}", value=f"{r.info.artifact_uri}/{artifact}")
+            tracker.log_output(name="SageMaker.ModelArtifact", value="NA")
